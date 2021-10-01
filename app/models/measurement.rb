4173ef3b5b28c369404347b6fe67bc6c690d95bd
FrederickMih <fredtimermih@gmail.com>
2021-10-01 15:19:32 +0100

    Add name and image validation test for Measurement

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,6 +1,6 @@
 class Measurement < ApplicationRecord
     has_many :measures
 
-    validates :name, presence: true, length: { minimum: 3, maximum: 24 }
-    validates :image_url, presence: true
+    validates_presence_of :name true, length: { minimum: 3, maximum: 24 }
+    validates_presence_of :image_url true
 end
