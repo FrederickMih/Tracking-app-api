63d5465f919fda396c7b32603e936747bdcda7a0
FrederickMih <fredtimermih@gmail.com>
2021-10-01 15:33:25 +0100

    Measurement api done

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,6 +1,6 @@
 class Measurement < ApplicationRecord
     has_many :measures
 
-    validates_presence_of :name true, length: { minimum: 3, maximum: 24 }
-    validates_presence_of :image_url true
+    validates_presence_of :name, length: { minimum: 3, maximum: 24 }
+    validates_presence_of :image_url
 end
