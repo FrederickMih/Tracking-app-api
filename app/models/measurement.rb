d065af0eb69d26ef58e94b31994967e691706a3d
FrederickMih <fredtimermih@gmail.com>
2021-10-21 19:39:09 +0100

    Add another test for the model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,6 +1,6 @@
 class Measurement < ApplicationRecord
   has_many :measures
 
-  validates_presence_of :name, length: { minimum: 3, maximum: 24 }
+ validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates_presence_of :image_url
 end
--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Measure, type: :model do
+  measurement = Measurement.create!(name: 'Left-bicep', image_url: 'Left-bicep-photo')
   it { should belong_to(:measurement) }
   it { should validate_presence_of(:value) }
 end
