3d33c7d442c663bdcc075ad133410c209867a24f
FrederickMih <fredtimermih@gmail.com>
2021-10-21 19:50:57 +0100

    Modify the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  # namespace :api do
-  #   namespace :v1 do  
-      resources :measures
       resources :measurements
-      # get 'measurements/:id' => 'measurements#show'
     end
-#   end
-# end
