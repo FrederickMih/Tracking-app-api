d302e5b961a152868a9204b9f00e0d56639b5684
FrederickMih <fredtimermih@gmail.com>
2021-10-28 09:40:50 +0100

    Add the api/v1 path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+   namespace :api do
+    namespace :v1 do  
       resources :measurements, only: [:index, :create]
       get 'measurements/:id' => 'measurements#show'
       resources :measures 
     end
+  end
+end
