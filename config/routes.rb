74ac3f3e1983fa97b3dbb4e98132ddfc65c269e4
FrederickMih <fredtimermih@gmail.com>
2021-11-05 07:05:36 +0100

    Modify users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
       post 'auth/login', to: 'authentication#create'
       post 'signup', to: 'users#create'
-      resources :users
       resources :measurements, only: [:index, :create]
       get 'measurements/:id' => 'measurements#show'
       resources :measures, only: [:create, :update, :show] 
