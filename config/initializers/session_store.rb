c6915d1bfedbbf0036d82baa836a3df5bcca9b07
FrederickMih <fredtimermih@gmail.com>
2021-11-10 23:37:57 +0100

    Modify session_store.rb file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_body_parts_tracker', domain: 'guarded-sands-43543.herokuapp.com', :same_site => :none, :secure => :true, tld_length: 3
+  Rails.application.config.session_store :cookie_store, key: '_body_parts_tracker', domain: 'fred-body-track-app-backend.herokuapp.com', :same_site => :none, :secure => :true, tld_length: 3
 else
   Rails.application.config.session_store :cookie_store, key: 'body_parts_tracker'
 end
