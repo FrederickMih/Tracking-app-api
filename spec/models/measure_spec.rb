bc367884085abfcfe1b22b24e0ce013ba31ec6c1
FrederickMih <fredtimermih@gmail.com>
2021-11-20 04:54:56 +0100

    Add other validation and association test for user and measurement model

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Measure, type: :model do
   it { should belong_to(:user) }
-  it { should belong_to(:Measurement) }
+  it { should belong_to(:measurement) }
 
   it { should validate_presence_of(:data) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:measures).dependent(:destroy) }
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:password_confirmation) } 
+  it {should validate_length_of(:username).is_at_least(4).is_at_most(8) }
  
 end
